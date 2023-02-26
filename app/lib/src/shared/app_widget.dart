adb032df45caec0c51c1fe9dce1f459f6b4b0673
Matheus Hora <matheus11hora@gmail.com>
2023-02-26 01:33:21 -0300

    add

--- app/lib/src/shared/app_widget.dart
+++ app/lib/src/shared/app_widget.dart
@@ -1,12 +1,7 @@
-import 'package:cadrninho/main.dart';
-import 'package:cadrninho/src/configuration/configuration_page.dart';
-import 'package:cadrninho/src/home/edit_task_board_page.dart';
-import 'package:cadrninho/src/home/home_page.dart';
-import 'package:cadrninho/src/shared/store/app_store.dart';
-import 'package:cadrninho/src/shared/store/app_store.dart';
-import 'package:cadrninho/src/shared/themes/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:listinha/src/shared/stores/app_store.dart';
+import 'themes/themes.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
